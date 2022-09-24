73aa3caf6a1d4c8177d20bed8b917e126a96da6d
Echostorm <107306362+Echostorm44@users.noreply.github.com>
2022-09-24 09:09:00 -0400

    version update

--- QuickFixMyPic/tools/chocolateyinstall.ps1
+++ QuickFixMyPic/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/Echostorm44/QuickFixMyPic/releases/download/1.0.0/SetupQuickFixMyPic.msi'
-$url64      = 'https://github.com/Echostorm44/QuickFixMyPic/releases/download/1.0.0/SetupQuickFixMyPic.msi'
+$url        = 'https://github.com/Echostorm44/QuickFixMyPic/releases/download/1.0.1.0/SetupQuickFixMyPic.msi'
+$url64      = 'https://github.com/Echostorm44/QuickFixMyPic/releases/download/1.0.1.0/SetupQuickFixMyPic.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -12,9 +12,9 @@ $packageArgs = @{
 
   softwareName  = 'QuickFixMyPic*'
 
-  checksum      = '9A3A1AFAFA897B2AD2D92DEB7517171C704172BA4819243472BC434ECD32C1EA'
+  checksum      = '68C54E339C4CAB6F063A8A5E456C78C80AC0E12391DB70304C51CC36000E861D'
   checksumType  = 'sha256'
-  checksum64    = '9A3A1AFAFA897B2AD2D92DEB7517171C704172BA4819243472BC434ECD32C1EA'
+  checksum64    = '68C54E339C4CAB6F063A8A5E456C78C80AC0E12391DB70304C51CC36000E861D'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
