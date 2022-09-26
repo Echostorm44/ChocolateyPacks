188533e2658fb77d9fde4ff2485e1a56849c2a8c
Echostorm <107306362+Echostorm44@users.noreply.github.com>
2022-09-26 11:33:25 -0400

    trimmed uneeded stuff

--- QuickFixMyPic/tools/chocolateyinstall.ps1
+++ QuickFixMyPic/tools/chocolateyinstall.ps1
@@ -1,21 +1,16 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://github.com/Echostorm44/QuickFixMyPic/releases/download/1.0.1.0/SetupQuickFixMyPic.msi'
-$url64      = 'https://github.com/Echostorm44/QuickFixMyPic/releases/download/1.0.1.0/SetupQuickFixMyPic.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
   fileType      = 'MSI'
   url           = $url
-  url64bit      = $url64
 
   softwareName  = 'QuickFixMyPic*'
 
   checksum      = '68C54E339C4CAB6F063A8A5E456C78C80AC0E12391DB70304C51CC36000E861D'
   checksumType  = 'sha256'
-  checksum64    = '68C54E339C4CAB6F063A8A5E456C78C80AC0E12391DB70304C51CC36000E861D'
-  checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
